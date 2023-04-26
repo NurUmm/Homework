49f372dc68b2d17c40380b81609352bd944949e8
NurislanUmmatov <soldatov20172016@gmail.com>
2023-04-26 09:32:55 +0300

    Tasks from 2 seminar

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,15 +1,27 @@
-﻿int num1 = Convert.ToInt32(Console.ReadLine());
-int num2 = Convert.ToInt32(Console.ReadLine());
+﻿// Console.WriteLine("Введите 3х значное число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if (num < 100 || num > 999) Console.WriteLine("Вы ввели неверное значение.");
+// string sometext = num.ToString();
+// Console.WriteLine(sometext[1]);
 
 
-if (num1 % num2 == 0) 
+// Console.WriteLine("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if (num < 99) Console.WriteLine("Нет третьей цифры.");
+// else
+// {
+//     string result = num.ToString();
+//     Console.WriteLine(result[2]);
+// }
+
+Console.WriteLine("Введите число дня недели: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num <= 7)
 {
-    Console.WriteLine("Кратно");
-    Console.WriteLine(num1 / num2);
+    if (num <= 5) Console.WriteLine("Нет");
+if (num > 5 && num < 8) 
+    {
+        Console.WriteLine("Да.");
+    }
 }
-
-else 
-{
-    Console.WriteLine($"не кратно, остаток { num1 % num2}");
-    Console.WriteLine(num1 / num2);
-}
+else Console.WriteLine("В неделе только 7 дней");
