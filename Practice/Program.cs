4350f72da22014309035179c71a853595c32aa9e
NurislanUmmatov <soldatov20172016@gmail.com>
2023-04-26 09:28:41 +0300

    Second folder

---
+++ Practice/Program.cs
@@ -0,0 +1,15 @@
+﻿int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+
+if (num1 % num2 == 0) 
+{
+    Console.WriteLine("Кратно");
+    Console.WriteLine(num1 / num2);
+}
+
+else 
+{
+    Console.WriteLine($"не кратно, остаток { num1 % num2}");
+    Console.WriteLine(num1 / num2);
+}
