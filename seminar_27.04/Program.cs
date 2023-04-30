f02121c4beca51cdcd9959dc5f4d1613ba11f6f3
NurislanUmmatov <soldatov20172016@gmail.com>
2023-04-30 11:46:39 +0300

    Task1

--- seminar_27.04/Program.cs
+++ seminar_27.04/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите 5ти значное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num > 9999 && num < 100000) {
+    string poli = num.ToString();
+    if (poli[0] == poli[4] && poli[1]==poli[3]) {
+        Console.WriteLine("Число является палиндромом.");
+    }
+    else {
+        Console.WriteLine("Число не является палиндромом.");
+    }
+}
+else {
+    System.Console.WriteLine("Некорректный ввод. Введите 5ти значное число.");
+}
